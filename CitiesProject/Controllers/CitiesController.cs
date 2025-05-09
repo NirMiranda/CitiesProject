3e3fc728ab593818acefc0c811db3a01a38a3f28
Nir Miranda <nirmiranda2@gmail.com>
2025-05-09 19:31:36 +0300

    changed table look, added pagination and search

--- CitiesProject/Controllers/CitiesController.cs
+++ CitiesProject/Controllers/CitiesController.cs
@@ -4,9 +4,11 @@ using System.Data;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using CitiesProject.Models;
+using PagedList;
 
 namespace CitiesProject.Controllers
 {
@@ -15,23 +17,48 @@ namespace CitiesProject.Controllers
         private CitiesProjectDBEntities db = new CitiesProjectDBEntities();
 
         // GET: Cities
-        public ActionResult Index()
+        public ActionResult Index(string sortOrder, string currentFilter, string searchString, int? page)
         {
-            return View(db.Cities.ToList());
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+
+            if (searchString != null)
+                page = 1;
+            else
+                searchString = currentFilter;
+
+            ViewBag.CurrentFilter = searchString;
+
+            var cities = from c in db.Cities select c;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                string convertedHebrew = ConvertEngToHebKeyboard(searchString);
+                cities = cities.Where(c =>
+                    c.CityName.Contains(searchString) ||
+                    c.CityName.Contains(convertedHebrew));
+            }
+
+            cities = sortOrder == "name_desc"
+                ? cities.OrderByDescending(c => c.CityName)
+                : cities.OrderBy(c => c.CityName);
+
+            int pageSize = 5;
+            int pageNumber = (page ?? 1);
+
+            return View(cities.ToPagedList(pageNumber, pageSize));
         }
 
         // GET: Cities/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
-            {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
+
             City city = db.Cities.Find(id);
             if (city == null)
-            {
                 return HttpNotFound();
-            }
+
             return View(city);
         }
 
@@ -42,8 +69,6 @@ namespace CitiesProject.Controllers
         }
 
         // POST: Cities/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "Id,CityName")] City city)
@@ -62,20 +87,17 @@ namespace CitiesProject.Controllers
         public ActionResult Edit(int? id)
         {
             if (id == null)
-            {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
+
             City city = db.Cities.Find(id);
             if (city == null)
-            {
                 return HttpNotFound();
-            }
+
+            ViewBag.EditMode = true; // For optional UI indication
             return View(city);
         }
 
         // POST: Cities/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "Id,CityName")] City city)
@@ -93,14 +115,12 @@ namespace CitiesProject.Controllers
         public ActionResult Delete(int? id)
         {
             if (id == null)
-            {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
+
             City city = db.Cities.Find(id);
             if (city == null)
-            {
                 return HttpNotFound();
-            }
+
             return View(city);
         }
 
@@ -118,10 +138,26 @@ namespace CitiesProject.Controllers
         protected override void Dispose(bool disposing)
         {
             if (disposing)
-            {
                 db.Dispose();
-            }
             base.Dispose(disposing);
+        }
+
+        // === Helper: Convert English keyboard input to Hebrew layout ===
+        private string ConvertEngToHebKeyboard(string input)
+        {
+            var map = new Dictionary<char, char>()
+            {
+                {'q','/'}, {'w','\''}, {'e','ק'}, {'r','ר'}, {'t','א'}, {'y','ט'}, {'u','ו'}, {'i','ן'}, {'o','ם'}, {'p','פ'},
+                {'a','ש'}, {'s','ד'}, {'d','ג'}, {'f','כ'}, {'g','ע'}, {'h','י'}, {'j','ח'}, {'k','ל'}, {'l','ך'}, {';','ף'},
+                {'z','ז'}, {'x','ס'}, {'c','ב'}, {'v','ה'}, {'b','נ'}, {'n','מ'}, {'m','צ'}, {',','ת'}, {'.','ץ'}
+            };
+
+            var converted = new StringBuilder();
+            foreach (var c in input.ToLower())
+            {
+                converted.Append(map.ContainsKey(c) ? map[c] : c);
+            }
+            return converted.ToString();
         }
     }
 }
